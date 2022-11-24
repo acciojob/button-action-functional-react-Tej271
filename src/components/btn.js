8e05793a0f7b9e50160db405fa463606d439d6f6
Tej271 <utu8811@gmail.com>
2022-11-24 16:18:41 +0530

    third change assign

--- src/components/btn.js
+++ src/components/btn.js
@@ -1,18 +1,26 @@
 import React from "react";
 
 const btn = () => {
+  const [para, setPara] = React.useState(false);
+
   return (
     <div>
       <Button
         id="click"
         type="button"
         onClick={() => {
-          <p id="para" style={{ opacity: 1 }}>
-            Hello, I've learnt to use the full-stack evaluation tool. This makes
-            me so happy
-          </p>;
+          setPara(!para);
         }}
-      ></Button>
+      >
+        Click me
+      </Button>
+
+      {para ? (
+        <p id="para" style={{ opacity: 1 }}>
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
   );
 };
